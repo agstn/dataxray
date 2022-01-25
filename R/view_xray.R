385c769176d61c32ef884d47cc31893cd315f7a9
Agustin Calatroni <acalatro@ms.rhoworld.com>
2022-01-24 20:19:42 -0800

    update help page w/ diamonds dataset

--- R/view_xray.R
+++ R/view_xray.R
@@ -45,25 +45,13 @@ type_indicator <- function(value = c("NUM", "CHAR", "DT/TIME")) {
 #'
 #' @examples
 #'
-#' diamonds <- diamonds %>% 
-#'  mutate(price = structure(price, label = 'price in US dollars'),
-#'         carat = structure(carat, label = 'weight of the diamond'),
-#'         cut = structure(cut, label = 'quality of the cut (Fair, Good, Very Good, Premium, Ideal)'),
-#'         color = structure(color, label = 'diamond colour, from D (best) to J (worst)'),
-#'         clarity = structure(clarity, label = 'a measurement of how clear the diamond is (I1 (worst), SI2, SI1, VS2, VS1, VVS2, VVS1, IF (best))'),
-#'         x = structure(x, label = 'length in mm'),
-#'         y = structure(y, label = 'width in mm'),
-#'         z = structure(z, label = 'depth in mm'),
-#'         depth = structure(depth, label = 'total depth percentage = z / mean(x, y) = 2 * z / (x + y)'),
-#'         table = structure(table, label = 'width of top of diamond relative to widest point'))
-#'         
 #' diamonds %>% 
 #'  make_xray() %>% 
 #'  view_xray()
 #'
 #' diamonds %>% 
 #'  make_xray(by = 'cut') %>% 
-#'  view_xray(by = 'cut')
+#'  view_xray(by =)
 #'
 view_xray <- function(data_xray, data_xray_shared = NULL, by = NULL, elementId = NULL){
 
