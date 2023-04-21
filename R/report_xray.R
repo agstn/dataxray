f8ebef06dedd68135bd442f3206db9c20ec30511
Russ McRee <holisticinfosec@gmail.com>
2023-04-20 19:18:10 -0700

    Update report_xray.R to open report in browser tab
    Update report_xray.R to open report in browser tab automatically. Useful when running dataxray in a Jupyter or Colab notebook.

--- R/report_xray.R
+++ R/report_xray.R
@@ -40,15 +40,15 @@ report_xray <- function(data, by = NULL, data_name, study, loc = NULL){
   } 
   
   if(!dir.exists(loc)) stop (paste0(loc," is not a valid directory"))
-    
-    report_template <- system.file("templates/report_xray.rmd", package = "dataxray")
-    
-    report_out <- file.path(loc, paste0(study,"_",data_name,"_xray")) 
-    
-    params_in <- list(data = data,
-                      data_name = data_name,
-                      study = study,
-                      by = by)
+  
+  report_template <- system.file("templates/report_xray.rmd", package = "dataxray")
+  
+  report_out <- file.path(loc, paste0(study,"_",data_name,"_xray")) 
+  
+  params_in <- list(data = data,
+                    data_name = data_name,
+                    study = study,
+                    by = by)
   
   file.copy(report_template, paste0(report_out,".rmd"), overwrite = TRUE)
   
@@ -56,4 +56,7 @@ report_xray <- function(data, by = NULL, data_name, study, loc = NULL){
   rmarkdown::render(input = paste0(report_out,".rmd"), 
                     output_file = paste0(report_out,".html"),
                     params = params_in)
+  report2browser <- paste0(report_out,".html")
+  
+  browseURL(report2browser)
 }
